177fdff6e6b2c2fcd728e0a7722e90a7ffbce16a
Yeolnim <734975095@qq.com>
2021-09-05 14:27:28 +0800

    Update

---
+++ amazon-crawler/amazon_crawler.py
@@ -0,0 +1,97 @@
+# -*- coding: UTF-8 -*-
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import re
+from lxml import etree
+import time
+import xml.etree.ElementTree as ET
+
+url = "https://www.amazon.cn/%E5%AD%99%E5%AD%90%E5%85%B5%E6%B3%95-%E5%AD%99%E6%AD%A6/product-reviews/B0011CT3HW/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
+url_detail = "https://www.amazon.cn/%E5%AD%99%E5%AD%90%E5%85%B5%E6%B3%95-%E5%AD%99%E6%AD%A6/dp/B0011CT3HW/ref=cm_cr_arp_d_bdcrb_top?ie=UTF8"
+
+# def getHTMLText(url):
+#     try:
+#         r=requests.request("get",url,timeout=10)
+#         r.raise_for_status()
+#         r.encoding=r.apparent_encoding
+#         return r.text
+#     except:
+#         return "exception"
+
+# html=getHTMLText(url)
+# soup=BeautifulSoup(html,"lxml")# 对获取的网页内容按照特定的解析器解析
+
+data = requests.get(url_detail).text
+s = etree.HTML(data)
+
+html = requests.get(url_detail)
+
+press = re.findall('<b>出版社:</b> (.*?);', html.text, re.S)[0]
+name = s.xpath('//*[@id="productTitle"]/text()')[0]
+year = s.xpath('//*[@id="title"]/span[3]/text()')[0].replace('– ', '').split("年")[0]
+author = s.xpath('//*[@id="bylineInfo"]/span/a/text()')[0]
+
+browser = webdriver.Chrome()
+browser.get(url)
+
+review = ET.Element("review")
+review.tail = '\n'
+
+page = 0
+while page < 1000:
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+
+    data = requests.get(url).text
+    s = etree.HTML(data)
+
+    html = requests.get(url)
+
+    browser.find_element_by_xpath('//*[@id="cm_cr-pagination_bar"]/ul/li[2]/a').click()
+    page = page + 1
+    time.sleep(2)
+
+    all_divs = soup.find_all(class_='a-section review aok-relative')
+
+    for each_div in all_divs:
+        summary = each_div.find(
+            class_='a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold').find(
+            'span').get_text()
+        score = each_div.find(class_='a-icon-alt').get_text().split(".")[0]
+        date = each_div.find(class_='a-size-base a-color-secondary review-date').get_text()
+        text = each_div.find(class_='a-size-base review-text review-text-content').get_text()
+
+        print(summary, score, date, text)
+
+        item = ET.SubElement(review, "item")
+        item.tail = '\n'
+
+        press_et = ET.SubElement(item, "press")
+        name_et = ET.SubElement(item, "name")
+        year_et = ET.SubElement(item, "year")
+        author_et = ET.SubElement(item, "author")
+        summary_et = ET.SubElement(item, "summary")
+        score_et = ET.SubElement(item, "score")
+        date_et = ET.SubElement(item, "date")
+        text_et = ET.SubElement(item, "text")
+
+        press_et.text = press
+        name_et.text = name
+        year_et.text = year
+        author_et.text = author
+        summary_et.text = summary
+        score_et.text = score
+        date_et.text = date
+        text_et.text = text
+
+        press_et.tail = '\n'
+        name_et.tail = '\n'
+        year_et.tail = '\n'
+        author_et.tail = '\n'
+        summary_et.tail = '\n'
+        score_et.tail = '\n'
+        date_et.tail = '\n'
+        text_et.tail = '\n'
+
+        tree = ET.ElementTree(review)
+        tree.write("AmazonReview.xml", encoding="utf-8", xml_declaration=True)
---
+++ amazon-crawler/auto_translate.py
@@ -0,0 +1,161 @@
+import xml.etree.ElementTree as ET
+# -*- coding: utf-8 -*-
+import sys
+import re
+import requests
+import json
+import execjs
+import http.client
+import hashlib
+import json
+import urllib
+import time
+import random
+
+tree = ET.parse('AmazonReview.xml')
+root = tree.getroot()
+
+
+def baidu_translate(content, fromLang='zh', toLang='en'):
+    appid = '20151113000005349'
+    secretKey = 'osubCEzlGjzvw8qdQc41'
+    httpClient = None
+    myurl = '/api/trans/vip/translate'
+    q = content
+    # fromLang = 'ru'  # 源语言
+    # toLang = 'zh'  # 翻译后的语言
+    salt = random.randint(32768, 65536)
+    sign = appid + q + str(salt) + secretKey
+    sign = hashlib.md5(sign.encode()).hexdigest()
+    myurl = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(
+        q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(
+        salt) + '&sign=' + sign
+
+    try:
+        httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+        httpClient.request('GET', myurl)
+        # response是HTTPResponse对象
+        response = httpClient.getresponse()
+        jsonResponse = response.read().decode("utf-8")  # 获得返回的结果，结果为json格式
+        js = json.loads(jsonResponse)  # 将json格式的结果转换字典结构
+        dst = str(js["trans_result"][0]["dst"])  # 取得翻译后的文本结果
+        return dst
+    except Exception as e:
+        return e
+    finally:
+        if httpClient:
+            httpClient.close()
+            time.sleep(1)
+
+
+class Py4Js():
+
+    def __init__(self):
+        self.ctx = execjs.compile(""" 
+		function TL(a) { 
+		var k = ""; 
+		var b = 406644; 
+		var b1 = 3293161072; 
+
+		var jd = "."; 
+		var $b = "+-a^+6"; 
+		var Zb = "+-3^+b+-f"; 
+
+		for (var e = [], f = 0, g = 0; g < a.length; g++) { 
+			var m = a.charCodeAt(g); 
+			128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023), 
+			e[f++] = m >> 18 | 240, 
+			e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224, 
+			e[f++] = m >> 6 & 63 | 128), 
+			e[f++] = m & 63 | 128) 
+		} 
+		a = b; 
+		for (f = 0; f < e.length; f++) a += e[f], 
+		a = RL(a, $b); 
+		a = RL(a, Zb); 
+		a ^= b1 || 0; 
+		0 > a && (a = (a & 2147483647) + 2147483648); 
+		a %= 1E6; 
+		return a.toString() + jd + (a ^ b) 
+	}; 
+
+	function RL(a, b) { 
+		var t = "a"; 
+		var Yb = "+"; 
+		for (var c = 0; c < b.length - 2; c += 3) { 
+			var d = b.charAt(c + 2), 
+			d = d >= t ? d.charCodeAt(0) - 87 : Number(d), 
+			d = b.charAt(c + 1) == Yb ? a >>> d: a << d; 
+			a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d 
+		} 
+		return a 
+	} 
+	""")
+
+    def getTk(self, text):
+        return self.ctx.call("TL", text)
+
+
+def google_szn_trans_sentence(sentence, sl="zh-CN", tl="en"):
+    # 中：zh-CN，英：en
+    headers = {
+        'cookie': '_ga=GA1.3.1163951248.1511946285; NID=131=XX0_dJsOrF47GXs2WNtO1MXyKVCK39bW4HXS0XZZ3ZYHTvMGOz8CVJe1G2XVwAJNF9MYOb1ngCqa_NegB6db2kgJ5A9hT3SScy0ag_L41wvtXHiPpNZweONFGHFNtWR_; 1P_JAR=2018-6-7-15',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+    }
+
+    url = 'https://translate.google.cn/translate_a/single?client=t&sl=' + sl + '&tl=' + tl + '&hl=' + tl \
+          + '&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=2&tk={tk}&q=' + sentence
+
+    js = Py4Js()
+    tk = js.getTk(sentence)
+    url = url.format(tk=tk)
+    # while
+    result = ""
+    try:
+        s = requests.get(url, headers=headers)
+        sj_lst = s.json()
+        if len(sj_lst[0]) == 1:
+            result = sj_lst[0][0][0]
+        else:
+            for x in sj_lst[0][:-1]:
+                result += x[0]
+    except:
+        pass
+    return result
+
+
+if __name__ == '__main__':
+
+    for item in root.findall('item'):
+        summary = item.find('summary').text
+        text = item.find('text').text
+        num = item.find('num').text
+        # print(summary, '\n', text)
+
+        try:
+            if text == None:
+                summary_trans = google_szn_trans_sentence(summary, tl="en")
+                text_trans = ""
+            else:
+                summary_trans = google_szn_trans_sentence(summary, tl="en")
+                text_trans = google_szn_trans_sentence(text, tl="en")
+                if text_trans == "":
+                    text_trans = baidu_translate(text, fromLang='zh', toLang='en')
+
+            print(num, '\n', summary_trans, '\n', text_trans)
+
+            summarytrans = ET.Element("summary_trans")
+            texttrans = ET.Element("text_trans")
+
+            summarytrans.text = summary_trans
+            texttrans.text = text_trans
+
+            summarytrans.tail = '\n'
+            texttrans.tail = '\n'
+
+            item.append(summarytrans)
+            item.append(texttrans)
+
+            tree.write("AmazonReview.xml", encoding="utf-8", xml_declaration=True)
+        except Exception as e:
+            print(e)
